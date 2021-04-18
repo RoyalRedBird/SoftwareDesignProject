b4eac1da2e1dbc46cea77d06fad3188ff5aae7c0
RoyalRedBird <33841152+RoyalRedBird@users.noreply.github.com>
2021-04-17 20:37:34 -0400

    Added Changes from Melvin

---
+++ project/Blackjack.java
@@ -0,0 +1,71 @@
+
+import java.util.*;
+
+/**
+ * @author melvi
+ */
+public class Blackjack {
+
+    public static void main(String[] args) {
+        ArrayList<Game> game = new ArrayList<Game>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("-=-=-=-=-=-=-= Welcome to the game of Blackjack! =-=-=-=-=-=-=-");
+        while (true) { // handles the beginning of the game
+            System.out.println("How many players are playing today?");
+            try {
+                int playerCount = Integer.parseInt(input.nextLine());
+                if (playerCount < 1 || playerCount > 5) {
+                    System.out.println("***** Minimum amount of players needs to be 1,"
+                            + " and maximum players cannot exceed 5");
+                } else {
+                    for (int i = 0; i < playerCount; i++) { // creates game objects and players
+                        game.add(new Game());
+                        System.out.println("Enter a custom name. If you want a default name, leave blank");
+                        String custom = input.nextLine();
+                        if (custom == "") {
+                            custom = "Player " + i;
+                        }
+                        game.get(i).setBalance(25);
+                        // playerArray.add(custom)
+                    }
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Number of players must be a number");
+            }
+        }
+
+        while (true) { // handles the turn system
+            game.get(0).setBalance(0);
+            play(game);
+            break; // TEMPORARY, DELETE LATER
+        }
+    }
+
+    public static void play(ArrayList<Game> object) { // this is a test method for now
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+        do {
+            try {
+                int balance = object.get(i).getBalance();
+                if (balance == 0) {
+                    i++;
+                } else {
+                    System.out.println("Enter a bet");
+                    int bet = Integer.parseInt(input.nextLine());
+                    if (balance - bet < 0) {
+                        System.out.println("Bet cannot be negative");
+                    } else {
+                        object.get(i).setBalance((balance - bet));
+                        System.out.println("Your new balance is: " + object.get(i).getBalance());
+                        i++;
+                    }
+                }
+
+            } catch (Exception e) {
+                System.out.println("Your bet must be a number");
+            }
+        } while (i < object.size());
+    }
+
+}
--- project/Game.java
+++ project/Game.java
@@ -1,58 +1,59 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
-package ca.sheridancollege.project;
 
 import java.util.ArrayList;
 
-/**
- * The class that models your game. You should create a more specific child of this class and instantiate the methods
- * given.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- */
-public abstract class Game {
+public class Game {
 
-    private final String name;//the title of the game
-    private ArrayList<Player> players;// the players of the game
+	private String name;
+        private int hand_value;
+        private int rounds_won;
+        private int balance;
+        
+        public Game(){
 
-    public Game(String name) {
-        this.name = name;
-        players = new ArrayList();
-    }
+        }
+        
+	public String getName() {
+		return this.name;
+	}
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    /**
-     * @return the players of this game
-     */
-    public ArrayList<Player> getPlayers() {
-        return players;
-    }
+	public int getBalance() {
+		return this.balance;
+	}
 
-    /**
-     * @param players the players of this game
-     */
-    public void setPlayers(ArrayList<Player> players) {
-        this.players = players;
-    }
+	/**
+	 * 
+	 * @param balance
+	 */
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
 
-    /**
-     * Play the game. This might be one method or many method calls depending on your game.
-     */
-    public abstract void play();
+	public int getHand_value() {
+		return this.hand_value;
+	}
 
-    /**
-     * When the game is over, use this method to declare and display a winning player.
-     */
-    public abstract void declareWinner();
+	/**
+	 * 
+	 * @param hand_value
+	 */
+	public void setHand_value(int hand_value) {
+		this.hand_value = hand_value;
+	}
 
-}//end class
+	public int getRounds_won() {
+		return this.rounds_won;
+	}
+
+	/**
+	 * 
+	 * @param rounds_won
+	 */
+	public void setRounds_won(int rounds_won) {
+		this.rounds_won = rounds_won;
+	}
+
+}
