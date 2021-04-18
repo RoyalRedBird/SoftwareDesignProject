8f1aa759fa8e3eb55880afcd42e3593011303ebd
Melvin-D01 <melvin.doroteo@ymail.com>
2021-04-17 20:39:11 -0400

    fixed betting

---
+++ Game.java
@@ -0,0 +1,59 @@
+
+import java.util.ArrayList;
+
+public class Game {
+
+	private String name;
+        private int hand_value;
+        private int rounds_won;
+        private int balance;
+        
+        public Game(){
+
+        }
+        
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBalance() {
+		return this.balance;
+	}
+
+	/**
+	 * 
+	 * @param balance
+	 */
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public int getHand_value() {
+		return this.hand_value;
+	}
+
+	/**
+	 * 
+	 * @param hand_value
+	 */
+	public void setHand_value(int hand_value) {
+		this.hand_value = hand_value;
+	}
+
+	public int getRounds_won() {
+		return this.rounds_won;
+	}
+
+	/**
+	 * 
+	 * @param rounds_won
+	 */
+	public void setRounds_won(int rounds_won) {
+		this.rounds_won = rounds_won;
+	}
+
+}
